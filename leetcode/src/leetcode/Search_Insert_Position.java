66f2bbe8f85f235890c9086f0a23897732673201
wayne chen <133223266+Wayne-chen0817@users.noreply.github.com>
2023-06-18 01:28:23 +0800

    Update Search_Insert_Position.java

--- leetcode/src/leetcode/Search_Insert_Position.java
+++ leetcode/src/leetcode/Search_Insert_Position.java
@@ -14,27 +14,27 @@ public class Search_Insert_Position {
 	}
 	
 	public static int searchInsert(int[] nums, int target) {
-        if(binarysearch(nums, target) >= 0) {//如果找不到會回傳負的
-        	return binarysearch(nums, target);
-        }
-        else
-        	return Math.abs(binarysearch(nums, target)) - 1; 
-    }
+        	if(binarysearch(nums, target) >= 0) {//如果找不到會回傳負的
+        		return binarysearch(nums, target);
+        	}
+        	else
+        		return Math.abs(binarysearch(nums, target)) - 1; 
+    	}
 	
 	public static int binarysearch(int[] nums, int target) {
 		int min = 0, max = nums.length - 1;
 		while(max >= min) {
-        	int mid = (min + max) / 2;
-        	if(nums[mid] == target) {
-        		return mid;
+        		int mid = (min + max) / 2;
+        		if(nums[mid] == target) {
+        			return mid;
+        		}
+        		else if(nums[mid] > target) {
+        			max = mid - 1;
+        		}
+        		else if(nums[mid] < target) {
+        			min = mid + 1;
+        		}
         	}
-        	else if(nums[mid] > target) {
-        		max = mid - 1;
-        	}
-        	else if(nums[mid] < target) {
-        		min = mid + 1;
-        	}
-        }
 		
 		return -1 - min;
 	}
