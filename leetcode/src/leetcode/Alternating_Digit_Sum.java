9ff6b03f60bcf442d505dede6ea2ee7a0594e1d6
wayne chen <133223266+Wayne-chen0817@users.noreply.github.com>
2023-06-18 00:55:44 +0800

    Update Alternating_Digit_Sum.java

--- leetcode/src/leetcode/Alternating_Digit_Sum.java
+++ leetcode/src/leetcode/Alternating_Digit_Sum.java
@@ -1,5 +1,10 @@
 package leetcode;
-
+/* 
+given a positive integer n. Each digit of n has a sign according to the following rules:
+The most significant digit is assigned a positive sign.
+Each other digit has an opposite sign to its adjacent digits.
+Return the sum of all digits with their corresponding sign. 
+*/
 public class Alternating_Digit_Sum {
 
 	public static void main(String[] args) {
