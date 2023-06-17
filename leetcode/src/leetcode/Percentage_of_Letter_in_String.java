b800cac55afadb7c1cb079bf2f725f7045653ad0
wayne chen <133223266+Wayne-chen0817@users.noreply.github.com>
2023-06-18 01:24:23 +0800

    Update Percentage_of_Letter_in_String.java

--- leetcode/src/leetcode/Percentage_of_Letter_in_String.java
+++ leetcode/src/leetcode/Percentage_of_Letter_in_String.java
@@ -10,12 +10,12 @@ public class Percentage_of_Letter_in_String {
 	}
 	
 	public static int percentageLetter(String s, char letter) {
-        double count = 0;
-        for(char c : s.toCharArray()) {
-        	if(letter == c) {
-        		count++;
+        	double count = 0;
+        	for(char c : s.toCharArray()) {
+        		if(letter == c) {
+        			count++;
+        		}
         	}
-        }
-        return (int)(count / s.length() * 100);
-    }
+        	return (int)(count / s.length() * 100);
+    	}
 }
