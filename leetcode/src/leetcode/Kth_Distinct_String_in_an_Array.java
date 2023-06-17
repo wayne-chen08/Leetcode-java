c13273002c69b56d13ffd6edb086d7f68d1957fc
wayne chen <133223266+Wayne-chen0817@users.noreply.github.com>
2023-06-18 01:43:09 +0800

    Update Kth_Distinct_String_in_an_Array.java

--- leetcode/src/leetcode/Kth_Distinct_String_in_an_Array.java
+++ leetcode/src/leetcode/Kth_Distinct_String_in_an_Array.java
@@ -1,4 +1,11 @@
 package leetcode;
+/*A distinct string is a string that is present only once in an array.
+
+Given an array of strings arr, and an integer k, return the kth distinct string present in arr. 
+If there are fewer than k distinct strings, return an empty string "".
+
+Note that the strings are considered in the order in which they appear in the array.
+*/
 import java.util.ArrayList;
 public class Kth_Distinct_String_in_an_Array {
 
