e95a7e674d332aa0f160f4a8f0cfd7dcab014a7d
wayne chen <133223266+Wayne-chen0817@users.noreply.github.com>
2023-06-18 01:25:27 +0800

    Update Reformat_Date.java

--- leetcode/src/leetcode/Reformat_Date.java
+++ leetcode/src/leetcode/Reformat_Date.java
@@ -20,40 +20,40 @@ public class Reformat_Date {
 	}
 	
 	public static String reformatDate(String date) {
-        String year = date.split(" ")[2];
-        String month = date.split(" ")[1];
-        String day = date.split(" ")[0];
+        	String year = date.split(" ")[2];
+        	String month = date.split(" ")[1];
+        	String day = date.split(" ")[0];
         
-        var month_map = new HashMap<String, String>();{ //用來把月份的英文轉成數字
-        	{
-        		month_map.put("Jan", "01");
-        		month_map.put("Feb", "02");
-        		month_map.put("Mar", "03");
-        		month_map.put("Apr", "04");
-        		month_map.put("May", "05");
-        		month_map.put("Jun", "06");
-        		month_map.put("Jul", "07");
-        		month_map.put("Aug", "08");
-        		month_map.put("Sep", "09");
-        		month_map.put("Oct", "10");
-        		month_map.put("Nov", "11");
-        		month_map.put("Dec", "12");
+        	var month_map = new HashMap<String, String>();{ //用來把月份的英文轉成數字
+        		{
+        			month_map.put("Jan", "01");
+        			month_map.put("Feb", "02");
+        			month_map.put("Mar", "03");
+        			month_map.put("Apr", "04");
+        			month_map.put("May", "05");
+        			month_map.put("Jun", "06");
+        			month_map.put("Jul", "07");
+        			month_map.put("Aug", "08");
+        			month_map.put("Sep", "09");
+        			month_map.put("Oct", "10");
+        			month_map.put("Nov", "11");
+        			month_map.put("Dec", "12");
+        		}	
         	}
-        }
         
-        month = month_map.get(month);
-        String result_day = "";
-        for(int i = 0; i < 2; i++) {
-        	if(Character.isDigit(day.charAt(i))) {
-        		result_day += day.charAt(i);
+        	month = month_map.get(month);
+        	String result_day = "";
+        	for(int i = 0; i < 2; i++) {
+        		if(Character.isDigit(day.charAt(i))) {
+        			result_day += day.charAt(i);
+        		}
         	}
-        }
         
-        if(result_day.length() == 1) {
-        	result_day = "0" + result_day;
-        }
+        	if(result_day.length() == 1) {
+        		result_day = "0" + result_day;
+        	}
         
-        return year + "-" + month + "-" + result_day;
-    }
+        	return year + "-" + month + "-" + result_day;
+    	}
 
 }
