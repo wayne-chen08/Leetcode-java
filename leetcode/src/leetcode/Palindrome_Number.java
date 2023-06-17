97a4114647fcf85ab89fda8158171fa5a4ffa38c
wayne chen <133223266+Wayne-chen0817@users.noreply.github.com>
2023-06-18 01:24:04 +0800

    Update Palindrome_Number.java

--- leetcode/src/leetcode/Palindrome_Number.java
+++ leetcode/src/leetcode/Palindrome_Number.java
@@ -9,14 +9,14 @@ public class Palindrome_Number {
 	}
 	
 	public static boolean isPalindrome(int x) {
-        String y = Integer.toString(x);
-        String result = "";
-        for(int i = y.length() - 1; i >= 0; i--) {
-        	result += y.charAt(i);
-        }
-        if(result.equals(y))
-        	return true;
-        return false;
-    }
+       		String y = Integer.toString(x);
+        	String result = "";
+        	for(int i = y.length() - 1; i >= 0; i--) {
+        		result += y.charAt(i);
+        	}
+        	if(result.equals(y))
+        		return true;
+        	return false;
+    	}
 
 }
