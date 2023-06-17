93abe6d714740beb119998717407a0e6659e69bd
wayne chen <133223266+Wayne-chen0817@users.noreply.github.com>
2023-06-18 01:19:28 +0800

    Update Sqrt.java

--- leetcode/src/leetcode/Sqrt.java
+++ leetcode/src/leetcode/Sqrt.java
@@ -1,5 +1,12 @@
 package leetcode;
+/*
+Given a non-negative integer x, return the square root of x rounded down to the nearest integer. 
+The returned integer should be non-negative as well.
 
+You must not use any built-in exponent function or operator.
+
+For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+*/
 public class Sqrt {//這題可以用binary search做
 
 	public static void main(String[] args) {
@@ -13,12 +20,12 @@ public class Sqrt {//這題可以用binary search做
 			return 1;
 		}
 		int n = 1;
-        for(long i = 1; i <= x / 2 + 1; i++) {
-        	if(i * i >= x) {
-        		if(i * i == x) {
-        			n = (int)i;
-        			break;
-        		}
+       		for(long i = 1; i <= x / 2 + 1; i++) {
+        		if(i * i >= x) {
+        			if(i * i == x) {
+        				n = (int)i;
+        				break;
+        			}
         		else {
         			n = (int)i - 1;
         			break;
