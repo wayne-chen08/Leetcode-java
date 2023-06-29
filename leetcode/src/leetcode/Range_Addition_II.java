e23e9b38668a06cbbfb3b2fe60e746164f7d3496
wayne chen <133223266+wayne-chen08@users.noreply.github.com>
2023-06-30 01:38:41 +0800

    Update Range_Addition_II.java

--- leetcode/src/leetcode/Range_Addition_II.java
+++ leetcode/src/leetcode/Range_Addition_II.java
@@ -1,8 +1,10 @@
 package leetcode;
-/*You are given an m x n matrix M initialized with all 0's and an array of operations ops, where ops[i] = [ai, bi] means M[x][y] should be incremented by one for all 0 <= x < ai and 0 <= y < bi.
+/*You are given an m x n matrix M initialized with all 0's and an array of operations ops, 
+where ops[i] = [ai, bi] means M[x][y] should be incremented by one for all 0 <= x < ai and 0 <= y < bi.
 
 Count and return the number of maximum integers in the matrix after performing all the operations.
 */
+//找出最小的x和y相乘
 public class Range_Addition_II {
 
 	public static void main(String[] args) {
